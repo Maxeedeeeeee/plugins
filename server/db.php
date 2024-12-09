bc176863c7027d0be4268e0d688fe3f15987f5e2
SharpyGH <110199236+Maxeedeeeeee@users.noreply.github.com>
2024-12-08 19:03:01 -0800

    Update db.php

--- server/db.php
+++ server/db.php
@@ -1,14 +1,14 @@
 <?php
-$host = 'localhost';
-$db = 'PvpClubPlugins';
-$user = 'root';
-$pass = '';
+$host = 'sql205.infinityfree.com'; // MySQL hostname
+$db = 'if0_37877650_mysql';       // MySQL database name
+$user = 'if0_37877650';           // MySQL username
+$pass = 'dudestopnow';     // Replace with the actual MySQL password
+$port = 3306;                     // MySQL port (optional, default is 3306)
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;port=$port", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Could not connect to the database: " . $e->getMessage());
 }
 ?>
-
